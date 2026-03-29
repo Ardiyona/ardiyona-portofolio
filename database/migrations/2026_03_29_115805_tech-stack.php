a97b636c394a15eb7a055726ae772736cb750ab0
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2026-03-29 19:18:47 +0700

    Modified Migration

--- database/migrations/2026_03_29_115805_tech-stack.php
+++ database/migrations/2026_03_29_115805_tech-stack.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tech-stack', function (Blueprint $table) {
+        Schema::create('tech-stacks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('techStack_code');
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tech-stack');
+        Schema::dropIfExists('tech-stacks');
     }
 };
--- database/migrations/2026_03_29_115927_portofolio.php
+++ database/migrations/2026_03_29_115927_portofolio.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('portofolio', function (Blueprint $table){
             $table->id();
-            $table->integer('category_id');
-            $table->integer('techStack_id');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('tech_stacks_id')->constrained('tech-stacks');
             $table->string('title');
             $table->text('description');
             $table->timestamp('date');
