849bdb49401822c0ff00da5b6f3fb15e2bf7196a
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2026-04-03 17:19:18 +0700

    change user model file name

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class UserModel extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
